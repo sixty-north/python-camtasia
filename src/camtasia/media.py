bb9580f2e0c19ee6fef28b9c835e17b263a3d6d4
Austin Bingham <austin.bingham@gmail.com>
2019-06-07 14:26:53 +0200

    Correctly reporting media-marker time stamps.
    Before we weren't properly taking into account the media-start and
    start elements (it's a bit involved).

--- src/camtasia/media.py
+++ src/camtasia/media.py
@@ -10,13 +10,25 @@ class Media:
     @property
     def markers(self):
         for m in self._data['parameters']['toc']['keyframes']:
+            marker_offset = FrameStamp(frame_number=m['time'],
+                                       frame_rate=self._frame_rate)
+
+            # The frame-stamps for media markers are stored relative to the media-start of the media, so we have to add
+            # media-start to the marker time to get the actual marker time.
+            print(marker_offset + self.media_start)
             yield Marker(name=m['value'],
-                         time=FrameStamp(frame_number=m['time'],
-                                         frame_rate=self._frame_rate))
+                         time=self.start + (marker_offset - self.media_start))
 
     @property
     def start(self):
+        "Where the media visible starts on the timeline."
         return FrameStamp(frame_number=self._data['start'],
+                          frame_rate=self._frame_rate)
+
+    @property
+    def media_start(self):
+        "The offset into the underlying media at which the media starts."
+        return FrameStamp(frame_number=self._data['mediaStart'],
                           frame_rate=self._frame_rate)
 
     @property
@@ -26,4 +38,3 @@ class Media:
 
     def __repr__(self):
         return f'Media(start={self.start}, duration={self.duration})'
-
