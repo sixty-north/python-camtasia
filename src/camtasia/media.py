6869de9fcb429137328e60c06267df6925f1a7a9
Austin Bingham <austin.bingham@gmail.com>
2019-06-07 14:37:50 +0200

    Added some documentation about media timestamps.

--- src/camtasia/media.py
+++ src/camtasia/media.py
@@ -3,6 +3,31 @@ from camtasia.marker import Marker
 
 
 class Media:
+    """Individual media elements on the timeline.
+
+    The relationship between the underlying media and that visible on the timeline on the timeline is a bit involved:
+
+             v media-start
+        -------------------------------------------------------
+        | underlying media                                    |
+        -------------------------------------------------------
+             |---- visible part of media ----|
+             |                               |
+             v start                         v
+    -------------------------------------------------------------------------------
+    |  timeline                                                                   |
+    -------------------------------------------------------------------------------
+
+    So `media-start` is the offset into the full, underlying media where the visble part starts.
+
+    `start` is the offset into the *timeline* where the visible part starts.
+
+    Media marker timestamps are calculated from the start of the underlying media. So in order to calculate the
+    timeline-relative timestamp for a media marker you need to take `start`, `media-start`, and the marker's timestamp
+    into account::
+
+        start + (marker_time - media_start)
+    """
     def __init__(self, media_data, frame_rate):
         self._data = media_data
         self._frame_rate = frame_rate
@@ -13,21 +38,19 @@ class Media:
             marker_offset = FrameStamp(frame_number=m['time'],
                                        frame_rate=self._frame_rate)
 
-            # The frame-stamps for media markers are stored relative to the media-start of the media, so we have to add
-            # media-start to the marker time to get the actual marker time.
             print(marker_offset + self.media_start)
             yield Marker(name=m['value'],
                          time=self.start + (marker_offset - self.media_start))
 
     @property
     def start(self):
-        "Where the media visible starts on the timeline."
+        "The offset on the timeline at which the visible media starts."
         return FrameStamp(frame_number=self._data['start'],
                           frame_rate=self._frame_rate)
 
     @property
     def media_start(self):
-        "The offset into the underlying media at which the media starts."
+        "The offset into the underlying media at which the visible media starts."
         return FrameStamp(frame_number=self._data['mediaStart'],
                           frame_rate=self._frame_rate)
 
