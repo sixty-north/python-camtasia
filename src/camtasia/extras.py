3f72725cab3b8678e56592ca91aeaac5f5e896ea
Austin Bingham <austin.bingham@gmail.com>
2019-07-11 12:24:20 +0200

    Fixes media_markers implementation.
    It was using an old API for tracks.

--- src/camtasia/extras.py
+++ src/camtasia/extras.py
@@ -12,7 +12,7 @@ def media_markers(project):
     """
     return (
         (marker, media, track)
-        for track in project.tracks
+        for track in project.timeline.tracks
         for media in track.medias
         for marker in media.markers
     )
