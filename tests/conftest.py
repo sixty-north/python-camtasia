a30f114602d78e45a170bc84833c61c799f029c4
Austin Bingham <austin.bingham@gmail.com>
2019-07-12 09:17:49 +0200

    Added a few project tests.

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,13 +2,22 @@ import pathlib
 
 import pytest
 
-from camtasia.project import Project
+from camtasia.project import load_project
 
 
 @pytest.fixture(scope='session')
-def simple_video():
-    "The 'simple-video' Project."
-
+def simple_video_path():
+    "Path to simple_video.cmproj."
     root = pathlib.Path(str(pytest.config.rootdir))
-    return Project(root / 'tests' / 'examples' / 'simple-video.cmproj')
+    return root / 'tests' / 'examples' / 'simple-video.cmproj'
 
+
+@pytest.fixture(scope='session')
+def simple_video(simple_video_path):
+    "The 'simple-video' Project."
+    return load_project(simple_video_path)
+
+
+@pytest.fixture
+def temp_path(tmpdir):
+    return pathlib.Path(str(tmpdir))
--- tests/test_project.py
+++ tests/test_project.py
@@ -1,3 +1,10 @@
+from camtasia import new_project, use_project
+
+
+def test_file_path(simple_video_path, simple_video):
+    assert simple_video_path.absolute() == simple_video.file_path.absolute()
+
+
 def test_authoring_client(simple_video):
     ac = simple_video.authoring_client
     assert ac.name == 'Camtasia'
@@ -7,3 +14,21 @@ def test_authoring_client(simple_video):
 
 def test_edit_rate(simple_video):
     assert simple_video.edit_rate == 30
+
+
+def test_use_project(simple_video_path):
+    with use_project(simple_video_path) as proj:
+        test_file_path(simple_video_path, proj)
+        test_authoring_client(proj)
+        test_edit_rate(proj)
+
+
+# TODO: Test use_project(save_on_exit=True/False)
+
+
+def test_new_project_creates_cmproj(temp_path):
+    project_path = temp_path / "temp.cmproj"
+    assert not project_path.exists()
+    new_project(project_path)
+    assert project_path.exists()
+    assert (project_path / "project.tscproj").exists()
