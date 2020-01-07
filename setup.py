1924b6f449ebd111dea866e3cd98983c4d2ec984
Austin Bingham <austin.bingham@gmail.com>
2020-01-07 12:41:45 +0100

    Bump version: 6.1.1 → 6.1.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='camtasia',
-    version="6.1.1",
+    version="6.1.2",
     packages=find_packages('src'),
 
     author='Sixty North AS',
