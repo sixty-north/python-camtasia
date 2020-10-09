708da6e48976b812b17055c60a130b8be0cbcdf0
Austin Bingham <austin.bingham@gmail.com>
2020-10-09 09:19:21 +0200

    Fixed test for client version.
    This is temporary. We need a better thought-out
    approach to Camtasia versions.

--- tests/test_project.py
+++ tests/test_project.py
@@ -9,7 +9,7 @@ def test_authoring_client(simple_video):
     ac = simple_video.authoring_client
     assert ac.name == 'Camtasia'
     assert ac.platform == 'Mac'
-    assert ac.version == '2020.0.8'
+    assert ac.version == '2019.0.1'
 
 
 def test_edit_rate(simple_video):
